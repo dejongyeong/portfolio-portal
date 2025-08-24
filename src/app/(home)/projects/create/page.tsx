cf38ac06a12de503a4c6449ea93ad5440c2cb5b3
dejongyeong <dejong.re2@gmail.com>
2025-08-24 14:40:36 +0100

    feat: add create project and publication page

---
+++ src/app/(home)/projects/create/page.tsx
@@ -0,0 +1,3 @@
+export default function CreateProjectPage() {
+  return <div className="bg-pink-300">Create Project</div>;
+}
---
+++ src/app/(home)/publications/create/page.tsx
@@ -0,0 +1,3 @@
+export default function CreatePublicationPage() {
+  return <div className="bg-pink-300">Create Publication</div>;
+}
